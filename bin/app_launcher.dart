31b0783102fc2aad1d4e581c9ec80e0f4df2adce
liudonghua123 <liudonghua123@gmail.com>
2021-03-19 15:01:15 +0800

    add initial code

--- bin/app_launcher.dart
+++ bin/app_launcher.dart
@@ -1,7 +1,22 @@
 import 'dart:io';
+import 'package:logger/logger.dart';
+import 'package:yaml/yaml.dart';
 
-import 'package:app_launcher/app_launcher.dart' as app_launcher;
+// import 'package:app_launcher/app_launcher.dart' as app_launcher;
 
 void main(List<String> arguments) {
-  print('Hello world: ${app_launcher.calculate()}!');
+  var logger = Logger();
+  var configFile = File('config.yaml');
+  if (!configFile.existsSync()) {
+    logger.e('config.yaml file not found!');
+    exit(-1);
+  }
+  var config = loadYaml(configFile.readAsStringSync());
+  logger.i('config: $config');
+  var result = Process.runSync(
+    config['app'],
+    (config['arguments'] as YamlList).value.map((e) => e.toString()).toList(),
+    runInShell: false,
+  );
+  logger.i('result: ${result.exitCode}, ${result.stdout}, ${result.stderr}');
 }
