40cfe5bf3dc1fb284743bb724db55e24efd0709d
liudonghua123 <liudonghua123@gmail.com>
2021-03-19 08:53:59 +0800

    initial files via `dart create -t console-full app_launcher`

---
+++ bin/app_launcher.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+import 'package:app_launcher/app_launcher.dart' as app_launcher;
+
+void main(List<String> arguments) {
+  print('Hello world: ${app_launcher.calculate()}!');
+}
---
+++ test/app_launcher_test.dart
@@ -0,0 +1,8 @@
+import 'package:app_launcher/app_launcher.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
